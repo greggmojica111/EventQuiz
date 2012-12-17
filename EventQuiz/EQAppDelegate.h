537247b74932c2d74aa65ef245177b5c626f3d20
Yael <yaelwb@gmail.com>
2012-12-16 19:57:12 -0800

    Initial Commit

---
+++ EventQuiz/EQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EQAppDelegate.h
+//  EventQuiz
+//
+//  Created by Yael Weinberg on 12/16/12.
+//  Copyright (c) 2012 Yael Weinberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EventQuiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EventQuiz
+//
+//  Created by Yael Weinberg on 12/16/12.
+//  Copyright (c) 2012 Yael Weinberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EQAppDelegate class]));
+    }
+}
